6f40934a7c8b4c8f9127e43a59c37a59ff65f70c
EniDev911 <Enidev911@gmail.com>
2020-10-07 05:45:20 -0400

    create task

--- flask_crud_task/main.py
+++ flask_crud_task/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +9,19 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+@app.route('/create-task', methods=['POST'])
+def create():
+    task = Task(content=request.form['task'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return 'saved'      
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
