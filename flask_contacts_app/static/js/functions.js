9c3517703d8b7e72ad4ee4e3b755dbb41eb5dc33
EniDev911 <Enidev911@gmail.com>
2020-10-06 23:36:58 -0400

    Update functions.js

--- flask_contacts_app/static/js/functions.js
+++ flask_contacts_app/static/js/functions.js
@@ -1,23 +1,5 @@
 const btnDelete = document.querySelectorAll('.btn-delete')
-/*
-Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire(
-      'Deleted!',
-      'Your file has been deleted.',
-      'success'
-    )
-  }
-})
-*/
+
 if(btnDelete){
   const btnArray = Array.from(btnDelete);
     btnArray.forEach((btn) => {
